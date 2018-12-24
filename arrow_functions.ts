118ef3a5a14be5c65b10f17bccae54cd80a9295c
programmervarunshridhar <46110005+programmervarunshridhar@users.noreply.github.com>
2018-12-24 22:24:12 +0530

    Update and rename arrow_functions to arrow_functions.ts

--- arrow_functions.ts
+++ arrow_functions.ts
@@ -3,5 +3,8 @@ const addNumbers = function (number1: number, number2: number): number {
 }
 
 const multiplyNumbers = (number1: number, number2: number) => number1 * number2;
-multiplyNumbers(10,20);
+multiplyNumbers(10, 20);
 
+const greet = () => console.log("hj");
+
+const returnName = (friend:string) => console.log(friend)
